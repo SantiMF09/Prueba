32f5cc82c8d86b331806cec798c4326c5e58ce9f
SantiMF09 <santiagomurillo1@gmail.com>
2020-01-28 09:26:21 +0100

    v2.0

--- Practicar JAVA/src/U4/T1.java
+++ Practicar JAVA/src/U4/T1.java
@@ -1,6 +1,7 @@
 package U4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class T1 
 {
@@ -15,6 +16,18 @@ public class T1
 		c = ran.nextInt(90-65+1)+65;
 
 		System.out.println("a= " +a+ " b= " +b+ " c= " +c);
+		
+		System.out.println("Introduzaca la cantidad de números a introducir: ");
+		Scanner teclado = new Scanner (System.in);
+		int n0;
+		n0= teclado.nextInt();
+		
+		System.out.println("-----------------------");
+		
+		System.out.println("Introduzaca el número");
+		Scanner teclado1 = new Scanner (System.in);
+		int n1;
+		n1= teclado.nextInt();
 	}
 
 }
